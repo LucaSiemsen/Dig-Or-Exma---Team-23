5b0a42c96bc608b8432a4c950f23cb2fb1ebc0a1
Dimitri H <dimitri.homutov@student.fh-kiel.de>
2025-11-28 05:12:08 +0100

    Basic timer für das Spiel

---
+++ src/timer.py
@@ -0,0 +1,21 @@
+#Erstellung eines Timers für das BAföG-System
+
+class BafoegTimer:
+    #Startmethode, die festlegt, wie lange der Timer läuft
+    def __init__(self, duration_seconds: float):
+        self.duration = duration_seconds  #Gesamtzeit des Timers
+        self.time_left = duration_seconds #Aktuell verbleibende Zeit
+
+    #verbleibende Zeit wird auf die ursprüngliche Dauer zurückgesetzt
+    def reset(self) -> None:
+        self.time_left = self.duration 
+        
+    #Methode, die die Zeit verringert
+    def update(self, dt: float) -> None:
+        self.time_left = max(0.0, self.time_left - dt)
+        #dt = vergangene Zeit seit letztem Frame.
+        #0.0 ist die Zeit, die minimal verbleiben kann (Timer läuft nicht ins Negative)
+
+    #Überprüfung, ob die Zeit abgelaufen ist
+    def is_over(self) -> bool:
+        return self.time_left <= 0.0
